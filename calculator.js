9f2ef649d0724b01ec137df5d66ea985b9696d94
arnavkulkarni0307 <68114100+arnavkulkarni0307@users.noreply.github.com>
2020-11-08 16:56:05 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,5 +1,5 @@
 function cretnumbers(value){
-var preval= document.getElementById('display').value;
+var preval= document.getElementById('display').innerHTML;
 if (preval == "0"){preval ='';
 
 
@@ -9,21 +9,16 @@ if (preval == "0"){preval ='';
 
 
 var newval = preval + value;
-document.getElementById('display').value = newval;}
+document.getElementById('display').innerHTML = newval;}
 
 function arnav(){
-document.getElementById('display').value = '0';}
+document.getElementById('display').innerHTML = '0';}
 
 function result(){
-	var calculation=document.getElementById('display').value;
+	var calculation=document.getElementById('display').innerHTML;
 	var results=eval(calculation);
-	document.getElementById('display').value = results;
+	document.getElementById('display').innerHTML = results;
 }
-
-
-
-
-
 
 
 
